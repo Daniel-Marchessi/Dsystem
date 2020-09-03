d5ba3191be728d1c5d80496b407453b4e6d707cf
Sergio.Dimier <sergiodimier@gmail.com>
2020-09-02 21:56:05 -0300

    slider

--- js/script2.js
+++ js/script2.js
@@ -6,6 +6,9 @@ var indexImagen = 0;
 var imageArray2 = ["img/Frontend_Store1.png", "img/Frontend_Store2.png", "img/Frontend_Store3.png"];
 var image2 = document.getElementById("imagen2");
 var indexI = 0;
+var imageArray3 = ["img/bienesraices-1.png", "img/bienesraices-2.png", "img/bienesraices-3.png", "img/bienesraices-4.png"];
+var image3 = document.getElementById("imagen3");
+var indexImagen3 = 0;
 
 function next(){
 indexImagen++;
@@ -28,7 +31,7 @@ if(indexImagen > 0){
 image.setAttribute("src", imageArray[indexImagen]);
 }
 
-function next1(){
+    function next1(){
     indexI++;
     if(indexI >= imageArray2.length){
         indexI = 0;
@@ -72,3 +75,26 @@ function next1(){
             showSlides();
         }
     console.log('funcionando');
+
+
+
+function next3(){
+    indexImagen3++;
+    if(indexImagen3 >= imageArray3.length){
+        indexImagen3 = 0;
+    }
+        image3.setAttribute("src", imageArray3[indexImagen3]);
+    
+    
+    }
+    
+    function previous3(){
+    
+    if(indexImagen3 > 0){
+        indexImagen3--;
+    }else{
+        indexImagen3 = imageArray3.length - 1;
+        
+    }
+    image3.setAttribute("src", imageArray3[indexImagen3]);
+    }
